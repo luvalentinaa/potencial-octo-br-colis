73810def1d6dad1dd113e864f3aca6549157836c
Lu Valentina <luvalentinaa29@gmail.com>
2022-04-06 21:53:13 -0300

    meu primeiro commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import { useState } from "react"
+import './index.css'
 
 
 const Home = () => {
   return (
     <div>
+    <div className="home">
       <h1>Home page</h1>
       <Contador/>
+    </div>
     </div>
   )
   
